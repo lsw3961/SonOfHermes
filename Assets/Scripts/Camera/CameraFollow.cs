55e0daf4200fc8c25a63f4f3778b757cb425416c
Logan White <lsw3961@g.rit.edu>
2022-02-24 23:42:45 -0500

    Camera Move
    The Camera moves when the player does, needs more testing vertically but right now works horizontally. Will test and tweak as time goes on.

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -4,18 +4,58 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    [SerializeField] private Transform target;
-    [SerializeField] private Vector3 offset;
-    [SerializeField] private float damping;
-    private Vector3 velocity = Vector3.zero;
+    [SerializeField] GameObject player;
+    [SerializeField] Camera cam;
+    [SerializeField] Vector2 camOffset = new Vector2(20,20);
+    [SerializeField] float yOffset = 15;
 
     private void FixedUpdate()
     {
-        Vector3 movePosition = target.position + offset;
-        if (target.localScale.x <0) 
+        StartCoroutine(CheckCam());
+    }
+    IEnumerator CheckCam()
+    {
+        //check the camera bounds
+        yield return new WaitForSeconds(0.1f);
+        Vector3 screenPos = cam.WorldToScreenPoint(player.transform.position);
+        float ratioX = screenPos.x / cam.pixelWidth;
+        float ratioY = screenPos.y / cam.pixelHeight;
+
+        if (ratioX < 0f) // if we're below our safe frame
+            MoveCam(-1, 0);
+        else if (ratioX > 1f)
         {
-            movePosition = new Vector3(target.position.x-offset.x,movePosition.y,movePosition.z);        }
-        
-        transform.position = Vector3.SmoothDamp(transform.position,movePosition,ref velocity,damping);
+            MoveCam(1, 0);
+        }
+        if (ratioY < 0f) // if we're above our safe frame, return false        
+            MoveCam(0, -1);
+        else if (ratioY > 1f)
+        {
+            MoveCam(0, 1);
+        }
+
+    }
+    private void MoveCam(int leftRight, int upDown) 
+    {
+
+        if (leftRight < 0)
+        {
+            this.transform.position = new Vector3(player.transform.position.x - camOffset.x ,cam.transform.position.y,this.transform.position.z);
+        }
+        else if (leftRight > 0) 
+        {
+            this.transform.position = new Vector3(player.transform.position.x + camOffset.x, cam.transform.position.y , this.transform.position.z);
+        }
+
+        if (upDown < 0)
+        {
+            this.transform.position = new Vector3(player.transform.position.x, player.transform.position.y - camOffset.y, this.transform.position.z);
+        }
+        else if (upDown > 0) 
+        {
+            this.transform.position = new Vector3(player.transform.position.x, player.transform.position.y + camOffset.y, this.transform.position.z);
+        }
+
+
     }
 }
