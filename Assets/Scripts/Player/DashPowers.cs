ee464813be2be59696fa3c80593b7ef5a7af2284
Logan White <lsw3961@g.rit.edu>
2022-02-27 22:13:29 -0500

    Testing for Framerate and prep for abilites
    I did some testing for the framerate and I found that during a build my framerate is jumping from 60-200 frames so im not worrried during a build. But! during editor the framerate is around 30. Thats annoying as hell. whatever the reason I'm gunna have to figure it out. The game is smooth in build so its time to move on to adding new abilities. Tomorrow will be Adding the blast and the Ground Pound. I did some prep for it tonight by writing out all the methods I just need to populate them. I think I have a good number of abilities that enables the player to really put the hurt on their foes. Once these abilities are all squared away and I like the feel of them I think I am ready to make the first level.

--- Assets/Scripts/Player/DashPowers.cs
+++ Assets/Scripts/Player/DashPowers.cs
@@ -4,4 +4,40 @@ using UnityEngine;
 
 public class DashPowers : MonoBehaviour
 {
+
+    //if the user strikes the ground they launch a force outward that damages and knocks back foes
+    public void DashGroundPound() 
+    {
+    
+    }
+
+    //a basic attack that piggybacks off of the dash
+    public void DashAttack() 
+    {
+    
+    }
+
+    //Allows you to phase through certain objects to get to secret areas
+    public void PhaseDash() 
+    {
+    
+    }
+
+    //when you dash you send a wave of energy in front of you that can break structures and damage foes
+    public void DashBlast() 
+    {
+    
+    }
+
+    //When dashing against a wall you bounce off of it
+    public void DashBounce() 
+    {
+    
+    }
+
+    //press shift or maybe middle mouse click to unlease a blast of light. Limited ammo for use but very powerful
+    public void LightBlast() 
+    {
+    
+    }
 }
